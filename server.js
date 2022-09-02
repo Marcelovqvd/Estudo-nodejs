c2b6bbdff3f36249c71c5c61e761bbe2c077c4e9
Marcelo <marcelovqvd@gmail.com>
2022-09-02 17:32:04 -0300

    new branch created

--- server.js
+++ server.js
@@ -5,4 +5,4 @@ const app = express()
 app.use(express.json())
 app.use(routes)
 
-app.listen(3333, () => console.log('O servidor está rodando na porta 3333'))
+app.listen(3333, () => console.log('O servidor está rodando na porta 3333. Estou na branch dev'))
